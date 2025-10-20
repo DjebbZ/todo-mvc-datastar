888ac9b7c8ee7f3935b55401747a018a470c6d8b
DjebbZ <khalid.jebbari@gmail.com>
2025-10-20 15:49:07 +0200

    Add favicon from favicon.io

--- src/templates/index.tsx
+++ src/templates/index.tsx
@@ -13,6 +13,10 @@ export const Layout = (props: SiteData) => html`
         <meta property="og:title" content="${props.title}"/>
         <link rel="stylesheet" href="/static/base.css" />
         <link rel="stylesheet" href="/static/index.css" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/static/apple-touch-icon.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="/static/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="/static/favicon-16x16.png">
+        <link rel="manifest" href="/static/site.webmanifest">
     </head>
     <body>
         <section class="todoapp">
