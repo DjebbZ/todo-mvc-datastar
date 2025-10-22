21ba2cb0761d579f07b8cab31dc776c135e2c8d8
DjebbZ <khalid.jebbari@gmail.com>
2025-10-22 10:34:34 +0200

    Fix todo item UI with missing `<li>`

--- src/templates/Todos.tsx
+++ src/templates/Todos.tsx
@@ -4,12 +4,14 @@ import {TodoProps} from "./prop-types";
 const Todos = (props: TodoProps) => html`
     <ul class="todo-list">
         ${props.todos?.map(todo => html`
-            <div class="view">
-                <input class="toggle" type="checkbox" ${todo.done ? 'checked' : ''}>
-                <label>${todo.title}</label>
-                <button class="destroy"></button>
-            </div>
-            <input class="edit" value="${todo.title}">
+            <li>
+                <div class="view">
+                    <input class="toggle" type="checkbox" ${todo.done ? 'checked' : ''}>
+                    <label>${todo.title}</label>
+                    <button class="destroy"></button>
+                </div>
+                <input class="edit" value="${todo.title}">
+            </li>
         `)}
     </ul>
 `
