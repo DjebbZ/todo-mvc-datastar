61ae2c241946111fc6c49f710565b8172e0f0223
DjebbZ <khalid.jebbari@gmail.com>
2025-10-23 23:11:21 +0200

    Add `toggleAll` use-case and add type parameters to all functions

--- src/logic/todos_use-cases.ts
+++ src/logic/todos_use-cases.ts
@@ -1,31 +1,36 @@
-import {TodoCreateDtoType, TodoType} from "./types";
+import {TodoType} from "./types";
 import {randomUUIDv7} from "bun";
 import {Database} from "bun:sqlite";
 
-export function addTodo(db: Database, title: TodoCreateDtoType) {
+export function addTodo(db: Database, title: string) {
     const id = randomUUIDv7()
 
     const query =
-        db.query("INSERT INTO todos (id, title) VALUES ($id, $title)")
+        db.query<never, {$title: typeof title, $id: typeof id}>("INSERT INTO todos (id, title) VALUES ($id, $title)")
     query.run({$title: title, $id: id})
 }
 
 export function getTodos(db: Database): TodoType[] {
-    const query = db.query<TodoType>("SELECT * FROM todos")
+    const query = db.query<TodoType, []>("SELECT * FROM todos")
     return query.all()
 }
 
 export function deleteTodo(db: Database, id: string) {
-    const query = db.query("DELETE FROM todos WHERE id = $id")
+    const query = db.query<never, {$id: typeof id}>("DELETE FROM todos WHERE id = $id")
     query.run({$id: id})
 }
 
 export function toggleTodo(db: Database, id: string) {
-    const query = db.query("UPDATE todos SET done = NOT done WHERE id = $id")
+    const query = db.query<never, {$id: typeof id}>("UPDATE todos SET done = NOT done WHERE id = $id")
     query.run({$id: id})
 }
 
+export function toggleAll(db: Database, done: boolean) {
+    const query = db.query<never, {$done: typeof done}>("UPDATE todos SET done = $done")
+    query.run({$done: done})
+}
+
 export function editTodo(db: Database, id: string, title: string) {
-    const query = db.query("UPDATE todos SET title = $title WHERE id = $id")
+    const query = db.query<never, {$title:typeof title, $id: typeof id}>("UPDATE todos SET title = $title WHERE id = $id")
     query.run({$title: title, $id: id})
 }
